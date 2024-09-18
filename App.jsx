328679d2321a5e61ac4ed523042739985307d715
qadeer766 <148043734+qadeer766@users.noreply.github.com>
2024-09-18 18:10:23 +0500

    src

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./componenst/Navigation";
+import Hero from "./componenst/Hero";
+
+
+export default function Header() {
+    return (
+        <div>
+            <Navigation />
+            <Hero />
+        </div>
+    )
+}
